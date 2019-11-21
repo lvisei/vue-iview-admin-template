8c96a6c7ffbfa4a6689f83318e1bc13543d0ea72
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2019-11-21 22:12:50 +0800

    chore: update utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -152,45 +152,44 @@ export const getParams = url => {
   })
   return paramObj
 }
+
 /**
- * 去抖
- * @param {Function} func
- * @param {String} wait
- * @param {Boolean} immediate
+ * 防抖 首次不执行
+ * @param {Function} fn
+ * @param {Number} time
  * @returns
  */
-const debounce = (func, wait, immediate) => {
-  let timeout, args, context, timestamp, result
-
-  const later = function() {
-    // 据上一次触发时间间隔
-    const last = +new Date() - timestamp
-
-    // 上次被包装函数被调用时间间隔last小于设定时间间隔wait
-    if (last < wait && last > 0) {
-      timeout = setTimeout(later, wait - last)
-    } else {
-      timeout = null
-      // 如果设定为immediate===true，因为开始边界已经调用过了此处无需调用
-      if (!immediate) {
-        result = func.apply(context, args)
-        if (!timeout) context = args = null
-      }
-    }
-  }
+export const debounce = (fn, time) => {
+  let timeout
 
   return function(...args) {
-    context = this
-    timestamp = +new Date()
-    const callNow = immediate && !timeout
-    // 如果延时不存在，重新设定延时
-    if (!timeout) timeout = setTimeout(later, wait)
-    if (callNow) {
-      result = func.apply(context, args)
-      context = args = null
+    const context = this
+    if (timeout) {
+      clearTimeout(timeout)
     }
+    timeout = setTimeout(() => {
+      fn.apply(context, args)
+      timeout = null
+    }, time)
+  }
+}
 
-    return result
+/**
+ * 节流 首次不执行
+ * @param {Function} fn
+ * @param {Number} time
+ * @returns
+ */
+export const throttle = (fn, time) => {
+  let timeout
+
+  return function(...args) {
+    const context = this
+    if (timeout) return
+    timeout = setTimeout(() => {
+      fn.apply(context, args)
+      timeout = null
+    }, time)
   }
 }
 
