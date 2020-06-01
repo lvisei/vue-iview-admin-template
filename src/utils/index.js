d5cb159767eec09537c79403d96422d4c5d2a88d
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2020-06-01 23:41:15 +0800

    refactor: new helpers

--- src/utils/index.js
+++ src/utils/index.js
@@ -175,6 +175,108 @@ export const generateOrganizationTree = (list, config) => {
   return jsonTree
 }
 
+/**
+ * addFavorite 加URL加入到收藏夹
+ * @param {String} sURL
+ * @param {String} sTitle
+ */
+export const addFavorite = (sURL, sTitle) => {
+  try {
+    window.external.addFavorite(sURL, sTitle)
+  } catch (e) {
+    try {
+      window.sidebar.addPanel(sTitle, sURL, '')
+    } catch (e) {
+      alert('加入收藏失败，请使用Ctrl+D进行添加')
+    }
+  }
+}
+
+/**
+ * scrollToTop 滚动到顶部
+ */
+export const scrollToTop = () => {
+  const c = document.documentElement.scrollTop || document.body.scrollTop
+
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop)
+    window.scrollTo(0, c - c / 8)
+  }
+}
+
+/**
+ * formatDate 时间日期格式转换
+ * @param {String} format  "yyyy-MM-dd hh:mm:ss"
+ * @returns "2020-05-29 12:32:12"
+ */
+export const formatDate = format => {
+  const date = new Date()
+  const o = {
+    'M+': date.getMonth() + 1, //month
+    'd+': date.getDate(), //day
+    'h+': date.getHours(), //hour
+    'm+': date.getMinutes(), //minute
+    's+': date.getSeconds(), //second
+    'q+': Math.floor((date.getMonth() + 3) / 3), //quarter
+    S: date.getMilliseconds() //millisecond
+  }
+  if (/(y+)/.test(format))
+    format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+  for (let k in o) {
+    if (new RegExp('(' + k + ')').test(format))
+      format = format.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length)
+      )
+  }
+  return format
+}
+
+/**
+ * timeFormat 时间个性化输出
+ * @param {String} time
+ * @returns “XX分钟前”
+ */
+export const timeFormat = time => {
+  // 1、< 60s, 显示为“刚刚”
+  // 2、>= 1min && < 60 min, 显示与当前时间差“XX分钟前”
+  // 3、>= 60min && < 1day, 显示与当前时间差“今天 XX:XX”
+  // 4、>= 1day && < 1year, 显示日期“XX月XX日 XX:XX”
+  // 5、>= 1year, 显示具体日期“XXXX年XX月XX日 XX:XX”
+  let date = new Date(time),
+    curDate = new Date(),
+    year = date.getFullYear(),
+    month = date.getMonth() + 10,
+    day = date.getDate(),
+    hour = date.getHours(),
+    minute = date.getMinutes(),
+    curYear = curDate.getFullYear(),
+    curHour = curDate.getHours(),
+    timeStr
+
+  if (year < curYear) {
+    timeStr = year + '年' + month + '月' + day + '日 ' + hour + ':' + minute
+  } else {
+    const pastTime = curDate - date,
+      pastH = pastTime / 3600000
+
+    if (pastH > curHour) {
+      timeStr = month + '月' + day + '日 ' + hour + ':' + minute
+    } else if (pastH >= 1) {
+      timeStr = '今天 ' + hour + ':' + minute + '分'
+    } else {
+      const pastM = curDate.getMinutes() - minute
+      if (pastM > 1) {
+        timeStr = pastM + '分钟前'
+      } else {
+        timeStr = '刚刚'
+      }
+    }
+  }
+
+  return timeStr
+}
+
 export default {
   onEvent,
   offEvent,
