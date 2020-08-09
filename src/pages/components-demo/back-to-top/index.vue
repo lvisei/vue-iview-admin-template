cd29ca503acde4cf26c8852fb24dc07fa5d9e910
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2020-08-09 23:28:45 +0800

    feat: up demo page

--- src/pages/components-demo/back-to-top/index.vue
+++ src/pages/components-demo/back-to-top/index.vue
@@ -1,23 +1,45 @@
 <template>
   <i-card class="back-to-top" shadow>
     BackToTop
-    <!-- <BackToTop
+    <pre>
+      <code v-text='`<BackToTop
+        :custom-style="myBackToTopStyle"
+        :visibility-height="300"
+        :back-position="0"
+        transition-name="fade"
+      />`'>
+      </code>
+    </pre>
+    <pre>
+      <code>
+      myBackToTopStyle: {
+        right: '50px',
+        bottom: '100px',
+        width: '40px',
+        height: '40px',
+        borderRadius: '4px',
+        lineHeight: '45px', // 请保持与高度一致以垂直居中
+        background: '#e7eaf1' // 按钮的背景颜色
+      }
+      </code>
+    </pre>
+    <BackToTop
       :custom-style="myBackToTopStyle"
       :visibility-height="300"
       :back-position="0"
       transition-name="fade"
-    /> -->
+    />
   </i-card>
 </template>
 
 <script>
-// import BackToTop from '@/components/BackToTop'
+import BackToTop from '@/components/BackToTop'
 
 export default {
   name: 'BackToTopDemo',
 
   components: {
-    // BackToTop
+    BackToTop
   },
 
   filters: {},
@@ -58,4 +80,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.back-to-top {
+  height: 1200px;
+}
+</style>
