24e2f00298bb7ac89ce8af29fbde3494b86e7f5d
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2021-02-02 16:53:45 +0800

    chore:  update dev  packages version

--- src/permission/index.js
+++ src/permission/index.js
@@ -41,7 +41,7 @@ router.beforeEach(async (to, from, next) => {
           // generate accessible routes map based
           const accessRoutes = await store.dispatch('routes/getUserMenutree')
           // dynamically add accessible routes
-          router.addRoutes(accessRoutes)
+          accessRoutes.forEach(route => router.addRoute(route))
           // hack method to ensure that addRoutes is complete
           // set the replace: true, so the navigation will not leave a history record
           next({ ...to, replace: true })
